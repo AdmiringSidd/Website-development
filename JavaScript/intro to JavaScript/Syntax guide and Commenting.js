f74291476e223596a3774ef9f8a99e25110e197b
AdmiringSidd <admiringsidd@gmail.com>
2025-05-12 20:51:24 +0530

    Intro to javascript

---
+++ JavaScript/intro to JavaScript/Syntax guide and Commenting.js
@@ -0,0 +1,4 @@
+var number1=90;
+var number2=27;
+var Total=number1+number2;
+console.log(Total)
---
+++ JavaScript/intro to JavaScript/Use of Operators.js
@@ -0,0 +1,8 @@
+var a=27;
+a*=17;
+console.log(a)
+var b=27;
+b/=9;
+console.log(b)
+total=a-b;
+console.log(total)
