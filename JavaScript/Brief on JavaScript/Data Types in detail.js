de6ed4e353482af4654fead0064615b47a8731c1
AdmiringSidd <admiringsidd@gmail.com>
2025-06-04 20:48:15 +0530

    good good work done by me

---
+++ JavaScript/Brief on JavaScript/Data Types in detail.js
@@ -0,0 +1,17 @@
+var a = 8;
+var b = 8;
+var d = null;
+var e; 
+{
+    let a = 7;
+    const b = 10;
+}
+console.log("a and b are equal : " + (a == b));
+console.log("<br>");
+console.log("The value of d is : " + d);
+console.log("<br>");
+console.log("The value of e is : " + e);
+console.log("<br>");
+console.log("The value of a is : " + a);
+console.log("<br>");
+console.log("The value of b is : " + b);
---
+++ JavaScript/Brief on JavaScript/Generate random numbers using a while loop.js
@@ -0,0 +1,7 @@
+var text = "";
+var i = 4;
+while (i < 10) {
+    text += "<br>The number is " + i;
+    i++;
+}
+console.log(text);
