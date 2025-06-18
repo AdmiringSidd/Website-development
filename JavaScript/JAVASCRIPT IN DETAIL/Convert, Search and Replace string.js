5b19a0cf636744f437c4bdd447a9045345a3e0aa
AdmiringSidd <admiringsidd@gmail.com>
2025-06-18 20:49:54 +0530

    javascript

---
+++ JavaScript/JAVASCRIPT IN DETAIL/Convert, Search and Replace string.js
@@ -0,0 +1,10 @@
+var str = "hello i am the cr of my class";
+var search_position = str.search("I AM VERY FRIENDLY");
+var txt = str.replace("I will always do what the class really need");
+var x = 100;
+var y = 50;
+
+console.log(search_position);
+console.log(txt);
+
+console.log(Number(x) + Number(y));
---
+++ JavaScript/JAVASCRIPT IN DETAIL/error handling .js
@@ -0,0 +1,6 @@
+try {
+  addalert("welcome guest!");
+}
+catch (err) {
+  console.log(err.message);
+}
